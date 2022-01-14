4f9a325df41e37d4be81d42dde8037d4cec1b70d
noahazevedoribeiro <noah_azevedoribeiro@sluz.ch>
2022-01-14 20:59:28 +0100

    index-router-for-cookies-set

--- Programming-creation/index.js
+++ Programming-creation/index.js
@@ -3,25 +3,21 @@
 import { Application , Router, renderFileToString} from "./deps.js";
 const itemList = JSON.parse(Deno.readTextFileSync(Deno.cwd()+"/assets/products.json")); 
 const router = new Router();
-let totalItems = 0;
+let itemscookie = 0;
 
 router.get("/", async (context) => {
     try {
-        console.log("\nhome:\n");
-
-        // count total items and save to cookie
-        let totalItems = 0;
         for(let i = 0; i < itemList.length; i++)
         {
             if(context.cookies.get(itemList[i].id))
             {
-                totalItems += parseInt(context.cookies.get(itemList[i].id));
+                itemscookie += parseInt(context.cookies.get(itemList[i].id));
             }
         }
-        context.cookies.set("totalItems", totalItems);
+        context.cookies.set("itemscookie", itemscookie);
 
         context.response.body = await renderFileToString(Deno.cwd() + 
-        "/views/home.ejs", { itemList: itemList, totalItems: totalItems });
+        "/views/index.ejs", { itemList: itemList, itemscookie: itemscookie });
         context.response.type = "html";
     } catch (error) {
         console.log(error);
