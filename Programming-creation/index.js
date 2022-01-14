e68fe69393517aed97d13c557d42b57ad7dc38a9
noahazevedoribeiro <noah_azevedoribeiro@sluz.ch>
2022-01-14 23:01:18 +0100

    error: TS2339

--- Programming-creation/index.js
+++ Programming-creation/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import { Application , Router, renderFileToString} from "./deps.js";
-const itemList = JSON.parse(Deno.readTextFileSync(Deno.cwd()+"/assets/products.json")); 
+const itemList = JSON.parse(Deno.readTextFileSync(Deno.cwd()+"./assets/products.json")); 
 const router = new Router();
-let itemscookie = 0;
 
+let itemscookie = 0;
+console.log("test-before-loop-")
 router.get("/", async (context) => {
+    console.log("test-enter-router.get")
     try {
         for(let i = 0; i < itemList.length; i++)
         {
@@ -14,11 +16,13 @@ router.get("/", async (context) => {
                 itemscookie += parseInt(context.cookies.get(itemList[i].id));
             }
         }
+        console.log("after-loop")
         context.cookies.set("itemscookie", itemscookie);
 
         context.response.body = await renderFileToString(Deno.cwd() + 
-        "/views/index.ejs", { itemList: itemList, itemscookie: itemscookie });
+        "./Views/index.html", { itemList: itemList, itemscookie: itemscookie });
         context.response.type = "html";
+        //await include("Views/index.ejs")
     } catch (error) {
         console.log(error);
     }
